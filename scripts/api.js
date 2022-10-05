3f06401561687c331d423325c6ed5aa50b580458
Brian <14955584+brian-reeder@users.noreply.github.com>
2022-10-05 12:41:08 -0500

    Fix checkbox listener.
    + Fix check box listener.
    + Refactor regex.
    + Pass params to setFields.

--- scripts/api.js
+++ scripts/api.js
@@ -6,41 +6,50 @@ window.onload = () => {
 	$('#g-input').on('input', updateSliders);
 	$('#b-input').on('input', updateSliders);
 	
-	$('.target').each((e) => {
-		$(this).on('input', updateSliders);
-	});
+	$('.target').each( (i,e) => $(e).on('input', updateSliders) );
 	
 }
 
-function updateHex(e) {	
-	const hex = $('#hex-input').val();
-	const reHexColor = /^#?[0-9A-Fa-f]{6}$/;
-	if(hex.match(reHexColor)) {
-		const shortHex = hex.startsWith('#') ? hex.slice(1) : hex;
+function updateHex() {	
+	const octet = '([0-9A-Fa-f]{2})'
+	const reHexColor = new RegExp(`^#?${octet}${octet}${octet}$`);
+	
+	const matches = $('#hex-input').val().match(reHexColor);
+	if(matches) {
 		const nums = {
-			'r': parseInt(shortHex.slice(0,2), 16),
-			'g': parseInt(shortHex.slice(2,4), 16),
-			'b': parseInt(shortHex.slice(4,6), 16),
+			'r': parseInt(matches[1], 16),
+			'g': parseInt(matches[2], 16),
+			'b': parseInt(matches[3], 16),
 		};
 		
-		setFields(nums);
+		setFields({
+			'hex': '',
+			'rgb': numsToRGB(nums),
+			'sliders': nums,
+		});
 	}
 };
 
-function updateRGB(e) {
-	const rgb = $("#rgb-input").val();
-	const reRGBColor = /^rgb\(([1-9]?[\d]|[1][\d][\d]|[2][0-4][\d]|[2][5][0-5]),([1-9]?[\d]|[1][\d][\d]|[2][0-4][\d]|[2][5][0-5]),([1-9]?[\d]|[1][\d][\d]|[2][0-4][\d]|[2][5][0-5])\)/;
-	if(rgb.match(reRGBColor)) {
-		const splitNums = rgb.slice(4, rgb.length-1).split(',');
-		
+function updateRGB() {	
+	const octet = '([\\d]|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])';
+	const reRGB = new RegExp(`^rgb\\(${octet},${octet},${octet}\\)$`);
+	
+	const matches = $("#rgb-input").val().match(reRGB);
+	if(matches) {	
 		const nums = {
-			'r': splitNums[0],
-			'g': splitNums[1],
-			'b': splitNums[2],
-		};
-		
-		setFields(nums);
+			'r': matches[1],
+			'g': matches[2],
+			'b': matches[3],
+		}
+	
+		setFields({
+			'hex': numsToHex(nums),
+			'rgb': '',
+			'sliders': nums, 
+		});
 	}
+
+	return;
 }
 
 function updateSliders(e) {
@@ -50,32 +59,42 @@ function updateSliders(e) {
 		'b': $('#b-input').val(),
 	};
 	
-	setFields(nums);
-}
-
-function setFields(nums) {
-	const hex = numsToHex(nums);
-	
-	setHex(hex);
-	setRGB(numsToRGB(nums));
-	setSliders(nums);
-	
-	$('#title').css('color', hex);
-
-	$('.target').each(function(e) {
-		const target = `#${$(this).val()}`;
-		$(target).css('background-color', $(this).is(':checked') ? hex : '#F8F8F2');
+	setFields({
+		'hex': numsToHex(nums),
+		'rgb': numsToRGB(nums),
+		'sliders': nums,
 	});
 }
 
+function setFields(params) {
+
+	if(params['hex']) {
+		setHex(params['hex']);
+	}
+
+	if(params['rgb']) {
+		setRGB(params['rgb']);
+	}
+
+	if(params['sliders']) {
+		const hex = numsToHex(params['sliders']);
+		$('#title').css('color', hex);
+
+		$('.target').each(function(e) {
+			const target = `#${$(this).val()}`;
+			$(target).css('background-color', $(this).is(':checked') ? hex : '#F8F8F2');
+		});
+
+		setSliders(params['sliders']);
+	}
+}
+
 function setHex(hex) {
-	$('hex-input').val(hex);
-	$('#hex-button').css('background-color', hex);
+	$('#hex-input').val(hex);
 };
 
 function setRGB(rgb) {
 	$('#rgb-input').val(rgb);
-	$('#rgb-button').css('background-color', rgb);
 };
 
 function setSliders(nums) {
